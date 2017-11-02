b15a8fef0f062daee263aaff0d2921c306988217
Zarinaa14 <smal_david@mail.ru>
2017-11-02 21:44:34 +0200

    a
    a

--- 8lesson/script.js
+++ 8lesson/script.js
@@ -1,51 +1,62 @@
-var menu=function(){
-	var t=15,z=50,s=6,a;
-	function dd(n){this.n=n; this.h=[]; this.c=[]}
-	dd.prototype.init=function(p,c){
-		a=c; var w=document.getElementById(p), 
-		s=w.getElementsByTagName('ul'),
-		l=s.length, 
-		i=0;
-		for(i;i<l;i++){
-			var h=s[i].parentNode; 
-			this.h[i]=h;
-			this.c[i]=s[i];
-			h.onmouseover=new Function(this.n+'.st('+i+',true)');
-			h.onmouseout=new Function(this.n+'.st('+i+')');
+var Menu = document.getElementsByTagName("div")[0];
+Menu.style.height = "640px";
+var obj = [
+	{
+		title: "Masha",
+		result: function(){
+			alert("Hellow,Masha");
+		}
+	},
+	{
+		title: "Sveta",
+			result: function(){
+			alert("Hellow,Sveta");
+		}
+	},
+	{
+		title: "Katya",
+			result: function(){
+			alert("Hellow,Katay");
+		}
+	},
+	
+	{
+		title: "Nikita",
+			result: function(){
+			alert("Hellow,Nikita");
 		}
 	}
-	dd.prototype.st=function(x,f){
-		var c=this.c[x],
-		h=this.h[x],
-		p=h.getElementsByTagName('a')[0];
-		clearInterval(c.t); c.style.overflow='hidden';
-		if(f){
-			p.className+=' '+a;
-			if(!c.mh){c.style.display='block';
-			c.style.height=''; 
-			c.mh=c.offsetHeight; 
-			c.style.height=0}
-			if(c.mh==c.offsetHeight){
-				c.style.overflow='visible'
-				}
-			else{
-				c.style.zIndex=z; z++; c.t=setInterval(function(){sl(c,1)},t)
-				}
-		}else{p.className=p.className.replace(a,''); 
-		c.t=setInterval(function(){sl(c,-1)},t)}
-	}
-	function sl(c,f){
-		var h=c.offsetHeight;
-		if((h<=0&&f!=1)||(h>=c.mh&&f==1)){
-			if(f==1){c.style.filter=''; 
-			c.style.opacity=1;
-			c.style.overflow='visible'}
-			clearInterval(c.t);
-			return
+]
+var ContextMenu = document.createElement("div");
+ContextMenu.setAttribute("class","context");
+for(var i = 0; i < obj.length; i++){
+	var Element = document.createElement("div");
+	Element.innerHTML = "<p>" + obj[i].title + "</p>"; 
+	ContextMenu.appendChild(Element);
+}
+ContextMenu.addEventListener("click", function(e){
+	// console.log(e.target);
+	obj.forEach(function(elem){
+		if(elem.title == e.target.outerText){
+			elem.result();
 		}
-		var d=(f==1)?Math.ceil((c.mh-h)/s):Math.ceil(h/s), o=h/c.mh;
-		c.style.opacity=o; c.style.filter='alpha(opacity='+(o*100)+')';
-		c.style.height=h+(d*f)+'px'
-	}
-	return{dd:dd}
-}();
+	})
+})
+    Menu.addEventListener("contextmenu", function(e){
+	e.preventDefault();
+	ContextMenu.style.position = "absolute";
+	ContextMenu.style.left = e.clientX +  10 +'px';
+	ContextMenu.style.top = e.clientY + 10 + 'px';
+	
+
+	
+	ContextMenu.style.left = (document.documentElement.clientWidth > e.clientX + 150) ? e.clientX + 10 + "px" : e.clientX -145 + "px" ;
+	ContextMenu.style.top = (document.documentElement.clientHeight > e.clientY + 150) ? e.clientY + 10 + "px" : e.clientY -156 + "px" ;
+    this.appendChild(ContextMenu);
+
+	
+})
+
+Menu.addEventListener("click", function(){
+	ContextMenu.remove();
+})
