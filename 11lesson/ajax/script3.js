07a7fb1fca2acce8995efe16c61daf11659f5d18
Zarinaa14 <smal_david@mail.ru>
2017-11-14 11:31:14 +0200

    a
    a

--- 11lesson/ajax/script3.js
+++ 11lesson/ajax/script3.js
@@ -5,48 +5,44 @@ var settings =  {
 	},
 	"#two":{
 		path: 'two.html',
-        local: function(){
-			      div.innerHTML = this.responseText;
-			     bt.addEventListener('click', function(){
-		         var bt= document.getElementById('button');
-	             var bt1= document.getElementById('button1');
-		         bt1.addEventListener('click', function(){
-			     alert(input1.value);
-	})
-		  
-		})
-	}
-		
+
+		handler: function(){
+			div.innerHTML = this.responseText;
+			var input = document.getElementsByTagName('input')[0];
+		    var bt = document.getElementsByTagName('button')[0];
+		    bt.addEventListener('click',function(){
+			alert(input.value);
+			})
+		}
 	},
 	"#three":{
 		path: 'three.html'
 	},
 	"#four":{
-		path: 'four.html',
-		local: function(){
-
-			
-			
-			
-			
-		
-		}
+		path: 'four.html'
 	},
 	"#five":{
 		path: 'five.html',
-		local: function(){
-		}
+		
 	}
+}
 
- 
-	var change = function(){
-	var path = settings[location.hash].path;
-	var locals = settings[location.hash].local;
-	var xhr = new XMLHttpRequest; xhr.open("GET", path, true);
-	xhr.onload = function(){
-		div.innerHTML = this.responseText;
-		locals();
-	}
+var change = function(){
+	if(settings[location.hash].handler == null){
+		var path = settings[location.hash].path;
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET",path,true);
+		xhr.onload = function(){
+			div.innerHTML = this.responseText;
+    }
+    }
+    else{
+     var path = settings[location.hash].path;
+	  var xhr = new XMLHttpRequest();
+	  xhr.open("GET",path,true);
+	  xhr.onload = settings[location.hash].handler;
+
+    }
 	xhr.send(null);
 }
 window.onhashchange = function(){
