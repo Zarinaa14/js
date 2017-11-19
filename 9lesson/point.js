ae28ba4ed95679c8610ec0113ab2205380831c40
Zarinaa14 <smal_david@mail.ru>
2017-11-19 23:06:54 +0200

    a
    a

--- 9lesson/point.js
+++ 9lesson/point.js
@@ -2,7 +2,7 @@ var point = {
 	x: 0,
 	y: 0
 };
-var reg = /^[0-9]{1,}$/;
+//var reg = /^[0-9]{1,}$/;
 Object.defineProperty(point, "x", {
 	
 })
@@ -15,7 +15,7 @@ Object.defineProperty(point, "x", {
 		 
            
 	set: function(value){
-		if(match(reg).test(value))
+		if((/^[0-9]{1,}$/).test(value))
 			this.x = Number(value);
 	}
 })
@@ -23,13 +23,13 @@ point.__defineGetter__("y", function(){
 	return this.y;
 })
 point.__defineSetter__("y", function(value){
-	if((Match(reg)).test(value))
+	if((/^[0-9]{1,}$/).test(value))
 		this.y = Number(value);
 })
 
 Object.defineProperty(point, "distance", {
 	value: function(x, y){
-		if((Match(reg)).test(x) && (match(reg)).test(y))
+		if((/^[0-9]{1,}$/).test(x) && (/^[0-9]{1,}$/).test(y))
 			return Math.sqrt(Math.pow(x-this.x,2)+Math.pow(y-this.y,2));
 	}
 })
